8231ea22ba35d77679d57f714b79c2956df11b14
cheokjiade <cheokjiade@gmail.com>
2012-11-04 19:59:07 +0800

    use sets

--- src/com/oopj/entities/Course.java
+++ src/com/oopj/entities/Course.java
@@ -1,17 +1,19 @@
 package com.oopj.entities;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Course {
 	private String name;
 	private String id;
-	private List<Student> studentList;
+	private Set<Student> studentList;
 	private Professor professor;
 	public Course(String name, String id) {
 		this.name = name;
 		this.id = id;
-		studentList = new ArrayList<Student>();
+		studentList = new HashSet<Student>();
 		
 	}
 	public String getName() {
@@ -26,10 +28,10 @@ public class Course {
 	public void setId(String id) {
 		this.id = id;
 	}
-	public List<Student> getStudentList() {
+	public Set<Student> getStudentList() {
 		return studentList;
 	}
-	public void setStudentList(List<Student> studentList) {
+	public void setStudentList(Set<Student> studentList) {
 		this.studentList = studentList;
 	}
 	public Professor getProfessor() {
--- src/com/oopj/entities/CourseClass.java
+++ src/com/oopj/entities/CourseClass.java
@@ -1,5 +1,11 @@
 package com.oopj.entities;
 
+import java.util.Set;
+
 public abstract class CourseClass {
-	
+	private int maxSize;
+	private String id;
+	private String name;
+	private Course parentCourse;
+	private Set<Student> studentList;
 }
