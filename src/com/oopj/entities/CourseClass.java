c5ee0dd2a771b960b2a098797d6aa0e31b01c2ff
cheokjiade <cheokjiade@gmail.com>
2012-11-05 16:13:07 +0800

    oops

--- src/com/oopj/entities/CourseClass.java
+++ src/com/oopj/entities/CourseClass.java
@@ -1,5 +1,6 @@
 package com.oopj.entities;
 
+import java.util.List;
 import java.util.Set;
 
 public abstract class CourseClass {
@@ -7,12 +8,42 @@ public abstract class CourseClass {
 	private String id;
 	private String name;
 	private Course parentCourse;
-	private Set<Student> studentList;
+	private List<Student> studentList;
 	public CourseClass(int maxSize, String id, String name, Course parentCourse) {
 		this.maxSize = maxSize;
 		this.id = id;
 		this.name = name;
 		this.parentCourse = parentCourse;
+	}
+	public int getMaxSize() {
+		return maxSize;
+	}
+	public void setMaxSize(int maxSize) {
+		this.maxSize = maxSize;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Course getParentCourse() {
+		return parentCourse;
+	}
+	public void setParentCourse(Course parentCourse) {
+		this.parentCourse = parentCourse;
+	}
+	public List<Student> getStudentList() {
+		return studentList;
+	}
+	public void setStudentList(List<Student> studentList) {
+		this.studentList = studentList;
 	}
 	
 }
