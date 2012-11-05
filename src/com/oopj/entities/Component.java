6f70e280ae72b69472d3a1f1e6a662aea04f5902
June <jquak1@gmail.com>
2012-11-05 16:35:03 +0800

    components, coursework

--- src/com/oopj/entities/Component.java
+++ src/com/oopj/entities/Component.java
@@ -1,9 +1,72 @@
 package com.oopj.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Component {
 	private String id;
 	private String name;
 	private int totalScore;
 	private int weightage;
+	
+	private CourseWork parentCourseWork;
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getTotalScore() {
+		return totalScore;
+	}
+
+	public void setTotalScore(int totalScore) {
+		this.totalScore = totalScore;
+	}
+
+	public int getWeightage() {
+		return weightage;
+	}
+
+	public void setWeightage(int weightage) {
+		this.weightage = weightage;
+	}
+
+	public CourseWork getCourseWork() {
+		return parentCourseWork;
+	}
+
+	public void setCourseWork(CourseWork courseWork) {
+		this.parentCourseWork = courseWork;
+	}
+
+	public List<Result> getResult() {
+		return resultList;
+	}
+
+	public void setResult(List<Result> resultList) {
+		this.resultList = resultList;
+	}
+
+	private List<Result> resultList;
+	
+	public Component(String id, String name, int totalScore, int weightage){
+		this.setId(id);
+		this.setName(name);
+		this.setTotalScore(totalScore);
+		this.setWeightage(weightage);
+		this.resultList = new ArrayList<Result>();
+
+	}
 	
 }
--- src/com/oopj/entities/CourseWork.java
+++ src/com/oopj/entities/CourseWork.java
@@ -1,7 +1,56 @@
 package com.oopj.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CourseWork {
 	private String id;
 	private String name;
 	private int weightage;
+	
+	private List<Component> componentList;
+	private Course parentCourse;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getWeightage() {
+		return weightage;
+	}
+
+	public void setWeightage(int weightage) {
+		this.weightage = weightage;
+	}
+
+	public List<Component> getComponent() {
+		return componentList;
+	}
+
+	public void setComponent(List<Component> componentList) {
+		this.componentList = componentList;
+	}
+
+	public CourseWork(String id, String name, int weightage, Course parentCourse) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.weightage = weightage;
+		this.parentCourse = parentCourse;
+		this.componentList = new ArrayList<Component>();
+	}
+	
+
 }
