6e631ec57aa5eee5c139ad9178148b15997ad05f
June <jquak1@gmail.com>
2012-11-05 17:41:30 +0800

    a

--- src/com/oopj/entities/Component.java
+++ src/com/oopj/entities/Component.java
@@ -3,54 +3,18 @@ package com.oopj.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Component {
-	private String id;
-	private String name;
-	private int totalScore;
-	private int weightage;
+public class Component extends ExamComponent {
+	
 	
 	private CourseWork parentCourseWork;
-	private List<Result> resultList;
 	
 	public Component(String id, String name, int totalScore, int weightage){
 		this.setId(id);
 		this.setName(name);
 		this.setTotalScore(totalScore);
 		this.setWeightage(weightage);
-		this.resultList = new ArrayList<Result>();
+		this.setResultList(new ArrayList<Result>());
 
-	}
-	
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getTotalScore() {
-		return totalScore;
-	}
-
-	public void setTotalScore(int totalScore) {
-		this.totalScore = totalScore;
-	}
-
-	public int getWeightage() {
-		return weightage;
-	}
-
-	public void setWeightage(int weightage) {
-		this.weightage = weightage;
 	}
 
 	public CourseWork getCourseWork() {
@@ -60,14 +24,5 @@ public class Component {
 	public void setCourseWork(CourseWork courseWork) {
 		this.parentCourseWork = courseWork;
 	}
-
-	public List<Result> getResult() {
-		return resultList;
-	}
-
-	public void setResult(List<Result> resultList) {
-		this.resultList = resultList;
-	}
-
 
 }
---
+++ src/com/oopj/entities/ExamComponent.java
@@ -0,0 +1,46 @@
+package com.oopj.entities;
+
+import java.util.List;
+
+public abstract class ExamComponent {
+	private String id;
+	private String name;
+	private int totalScore;
+	private int weightage;
+	private List<Result> resultList;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getTotalScore() {
+		return totalScore;
+	}
+	public void setTotalScore(int totalScore) {
+		this.totalScore = totalScore;
+	}
+	public int getWeightage() {
+		return weightage;
+	}
+	public void setWeightage(int weightage) {
+		this.weightage = weightage;
+	}
+	
+	public List<Result> getResultList() {
+		return resultList;
+	}
+
+	public void setResultList(List<Result> resultList) {
+		this.resultList = resultList;
+	}
+	
+	
+}
