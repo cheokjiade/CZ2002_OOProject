508a572839a170aa7f0455939cfec1f07e40de53
Sri Hartati <cheezy90@hotmail.com>
2012-11-05 16:48:32 +0800

    Professor entity

--- src/com/oopj/entities/Professor.java
+++ src/com/oopj/entities/Professor.java
@@ -1,5 +1,25 @@
 package com.oopj.entities;
 
-public class Professor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Professor extends Person {
+	private List<Course> courseList;
+
+	public Professor(String name, String id) {
+		this.setName(name);
+		this.setId(id);
+		this.courseList = new ArrayList<Course>();
+	}
+	
+	public List<Course> getCourseList() {
+		return courseList;
+	}
+
+	public void setCourseList(List<Course> courseList) {
+		this.courseList = courseList;
+	}
+	
+	
 
 }
