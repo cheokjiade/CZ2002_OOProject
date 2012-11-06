f16322761e0af3c9bf231e1a6fb769e73692c899
June <jquak1@gmail.com>
2012-11-06 17:46:02 +0800

    aaaa

--- src/com/oopj/controllers/main.java
+++ src/com/oopj/controllers/main.java
@@ -312,19 +312,14 @@ public class main {
 		return null;
 	}
 	
-	public static void registerStudent(){
+public static void registerStudent(){
 		
-		for (int i=0;i<studentList.size();i++){
-			System.out.println(Integer.toString(i) + ". " + studentList.get(i).getId() + " " + studentList.get(i).getName());
-		}
 		System.out.println("Select student to register: ");
-		Student s = studentList.get(sc.nextInt());
+		Student s = (Student)chooseChoosable((ArrayList)studentList); 
 		
 		System.out.println("Select course to register student into: ");
-		for (int i=0;i<courseList.size();i++){
-			System.out.println(Integer.toString(i) + ". " + courseList.get(i).getId() + " " + courseList.get(i).getName());
-		}
-		Course c = courseList.get(sc.nextInt());
+		Course c = (Course)chooseChoosable((ArrayList)courseList);
+		
 		if(c.getStudentList().contains(s)){
 			System.out.println("Student is already enrolled.");
 			return;
@@ -334,24 +329,24 @@ public class main {
 		s.getCourseList().add(c);
 		c.getStudentList().add(s);
 
-
-		
-		System.out.println("Please select the class to register for...");
-		  for (int i=0;i<c.getCourseClassList().size();i++){
-		   System.out.println(Integer.toString(i) + ". " + c.getCourseClassList().get(i).getId() + " " + c.getCourseClassList().get(i).getName());
-		  }
-		CourseClass cc = c.getCourseClassList().get(sc.nextInt());
-		c.getCourseClassList().add(cc);
-
-		
-		System.out.println("Printing course list of student");
-		for(Course tempCourse : s.getCourseList()){
-			System.out.println(tempCourse.getId() + " " + tempCourse.getName());
+		for(int i=1;i<=3;i++){
+			ArrayList<CourseClass> tempCCList = new ArrayList<CourseClass>();
+			for(CourseClass tempCC: c.getCourseClassList()){ 
+				if(tempCC.getType()==i) {
+					tempCCList.add(tempCC);
+				}
+			}
+			if(tempCCList.size()>0){
+				System.out.println("Please select the class to register for...");
+				CourseClass cc = (CourseClass)chooseChoosable((ArrayList)tempCCList);
+				s.getCourseClassList().add(cc);
+				cc.getStudentList().add(s);
+			}
 		}
-		System.out.println("Printing students courses class");
-		for(Course c1 : s.getCourseList()){
-			System.out.println(s.getName() + " enrolled in " + cc.getName());
-		}
+		
+		db.store(s);
+		db.store(c);
+		
 		}
 	
 	public static void printStudentList() {
